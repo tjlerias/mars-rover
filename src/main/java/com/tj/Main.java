09c516d6b782bebcab1aa9b2170f01b2b9bae9c1
tjlerias <25985002+tjlerias@users.noreply.github.com>
2024-01-09 21:36:27 +0800

    Add user input

--- src/main/java/com/tj/Main.java
+++ src/main/java/com/tj/Main.java
@@ -1,5 +1,7 @@
 package com.tj;
 
+import java.util.Scanner;
+
 import static com.tj.Direction.N;
 
 public class Main {
@@ -7,6 +9,10 @@ public class Main {
     public static void main(String[] args) {
         RoverService roverService = new RoverService();
 
-        roverService.move(new Rover(0, 0, N), "LMLMLMLMLM");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input instructions: ");
+        String instruction = scanner.nextLine();
+
+        System.out.println("Output: " + roverService.move(new Rover(0, 0, N), instruction));
     }
 }
